008d1140d6300dc4bdcbdbef9f70c5016ee687d5
Ali <a80.abbasi@yahoo.com>
2020-04-12 02:25:29 +0430

    Completing sub class set filtering type

---
+++ src/main/java/View/SetFilteringTypeMenu.java
@@ -0,0 +1,63 @@
+package View;
+
+import java.util.ArrayList;
+
+public class SetFilteringTypeMenu extends Menu {
+    public SetFilteringTypeMenu(Menu parentMenu) {
+        super("Set Filtering Type Menu", parentMenu);
+        ArrayList<Menu> subMenus = new ArrayList<>();
+        subMenus.add(getCategoryFilterMenu());
+        subMenus.add(getNameFilteringMenu());
+        subMenus.add(getExistenceFilteringMenu());
+        this.setSubmenus(subMenus);
+    }
+
+    private Menu getCategoryFilterMenu(){
+        return new Menu("Category Filtering", this) {
+            @Override
+            public void execute() {
+                String categoryName = scanner.nextLine().trim();
+                try {
+                    productsManager.addCategoryFilter(categoryName);
+                }
+                catch (IllegalArgumentException e){
+                    System.out.println("there is no such a category!");
+                }
+                finally {
+                    parentMenu.execute();
+                }
+            }
+        };
+    }
+
+    private Menu getNameFilteringMenu(){
+        return new Menu("Name Filtering", this) {
+            @Override
+            public void execute() {
+                productsManager.addNameFiltering(scanner.nextLine().trim());
+                parentMenu.execute();
+            }
+        };
+    }
+
+    private Menu getExistenceFilteringMenu(){
+        return new Menu("Existence Filtering", this) {
+            @Override
+            public void execute() {
+                System.out.println("1. only existing files");
+                System.out.println("2. all of files");
+                String command = scanner.nextLine().trim();
+                if (command.equals("1") || command.equalsIgnoreCase("only existing files")){
+                    productsManager.addExistenceFilter(true);
+                }
+                else if (command.equalsIgnoreCase("all of files") || command.equals("2")){
+                    productsManager.addExistenceFilter(false);
+                }
+                else {
+                    System.out.println("wrong input!");
+                }
+                parentMenu.execute();
+            }
+        };
+    }
+}
