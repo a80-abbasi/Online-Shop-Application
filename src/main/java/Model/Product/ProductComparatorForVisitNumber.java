53778e12bba0bfcf3133c1c696c511a1f7f6c60b
Ali <a80.abbasi@yahoo.com>
2020-04-12 00:00:04 +0430

    Creating Comparator For sorting Products based on score

--- src/main/java/Model/Product/ProductComparatorForVisitNumber.java
+++ src/main/java/Model/Product/ProductComparatorForVisitNumber.java
@@ -6,9 +6,9 @@ public class ProductComparatorForVisitNumber implements Comparator<Product> {
 
     @Override
     public int compare(Product product1, Product product2) {
-        int visitNumber = product2.getVisitNumber() - product1.getVisitNumber();
-        if (visitNumber != 0){
-            return visitNumber;
+        int visitNumberDifference = product2.getVisitNumber() - product1.getVisitNumber();
+        if (visitNumberDifference != 0){
+            return visitNumberDifference;
         }
         return product1.getProductName().compareTo(product2.getProductName());
     }
