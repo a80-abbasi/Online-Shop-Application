7715e8d9edb03b11e04d66bef816ccea11910595
Ali <a80.abbasi@yahoo.com>
2020-04-12 23:16:23 +0430

    Completing sub class set sort type

---
+++ src/main/java/View/SetSortTypeMenu.java
@@ -0,0 +1,46 @@
+package View;
+
+import Controller.SortType;
+
+import java.util.ArrayList;
+
+public class SetSortTypeMenu extends Menu {
+    public SetSortTypeMenu(Menu parentMenu) {
+        super("Set Sort Type Menu", parentMenu);
+        ArrayList<Menu> subMenus = new ArrayList<>();
+        subMenus.add(getSortByTimeMenu());
+        subMenus.add(getSortByScore());
+        subMenus.add(getSortByVisitNumber());
+        this.setSubmenus(subMenus);
+    }
+
+    private Menu getSortByTimeMenu(){
+        return new Menu(SortType.SORT_BY_TIME.getSortType(), this) {
+            @Override
+            public void execute() {
+                productsManager.useSortByTime();
+                parentMenu.execute();
+            }
+        };
+    }
+
+    private Menu getSortByScore(){
+        return new Menu(SortType.SORT_BY_SCORE.getSortType(), this) {
+            @Override
+            public void execute() {
+                productsManager.useSortByScore();
+                parentMenu.execute();
+            }
+        };
+    }
+
+    private Menu getSortByVisitNumber(){
+        return new Menu(SortType.SORT_BY_VISIT.getSortType(), this) {
+            @Override
+            public void execute() {
+                productsManager.useSortByVisit();
+                parentMenu.execute();
+            }
+        };
+    }
+}
