e6add1e9b3ce912c7777a210c0ad16413e84a959
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-04-15 00:51:56 +0430

    Editing MainMenu.java and OffMenu.java

--- src/main/java/View/MainMenu.java
+++ src/main/java/View/MainMenu.java
@@ -1,7 +1,17 @@
 package View;
 
+import View.ProductMenus.ProductMenu;
+import View.ProductsMenus.ProductsMenu;
+
+import java.util.ArrayList;
+
 public class MainMenu extends Menu {
     public MainMenu() {
         super("Main Menu", null);
+        ArrayList<Menu> submenus = new ArrayList<>();
+        submenus.add(new LoginAndRegisterMenu(this));
+        submenus.add(new ProductsMenu(this));
+        submenus.add(new OffMenu(this));
+        this.setSubmenus(submenus);
     }
 }
--- src/main/java/View/OffMenu.java
+++ src/main/java/View/OffMenu.java
@@ -1,9 +1,37 @@
 package View;
 
+import java.util.ArrayList;
+
 public class OffMenu extends Menu{
 
     public OffMenu(Menu parentMenu) {
         super("Off Menu", parentMenu);
+        ArrayList<Menu> submenus = new ArrayList<>();
+        submenus.add(getShowProductMenu());
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    private Menu getShowProductMenu() {
+        return new Menu("Show Product", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
     }
 
 }
