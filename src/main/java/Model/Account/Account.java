862a544a8d687b45224305d3dbf255031a13b88b
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-04-15 00:32:28 +0430

    Editing Account.java and Admin.java

--- src/main/java/Model/Account/Account.java
+++ src/main/java/Model/Account/Account.java
@@ -2,37 +2,75 @@ package Model.Account;
 
 import java.util.ArrayList;
 
-
-
 public abstract class Account {
     private static ArrayList<Account> allAccounts = new ArrayList<>();
-    public ArrayList<Field> personalFields = new ArrayList<>();
+    private String username;
+    private String password;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
 
     public Account(String username, String password, String name, String lastName, String email, String phoneNumber) {
-        personalFields.add(new Field("username", username));
-        personalFields.add(new Field("password", password));
-        personalFields.add(new Field("name", name));
-        personalFields.add(new Field("lastName", lastName));
-        personalFields.add(new Field("email", email));
-        personalFields.add(new Field("phoneNumber", phoneNumber));
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
         allAccounts.add(this);
     }
 
-    public String getFieldValue(String name) {
-        for (Field personalField : personalFields) {
-            if (personalField.getName().equals(name)) {
-                return personalField.getValue();
-            }
-        }
-        return null;
+    public Account() {
+        this("", "", "", "", "", "");
     }
 
-    public void setFieldValue(String name, String newValue) {
-        for (Field personalField : personalFields) {
-            if (personalField.getName().equals(name)) {
-                personalField.setValue(newValue);
-            }
-        }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public static ArrayList<Account> getAllAccounts() {
@@ -41,21 +79,10 @@ public abstract class Account {
 
     public static Account getAccountByUsername (String username) {
         for (Account account : allAccounts) {
-            for (Field personalField : account.personalFields) {
-                if (personalField.getName().equals("username")) {
-                    return account;
-                }
+            if (account.getUsername().equalsIgnoreCase(username)) {
+                return account;
             }
         }
         return null;
-    }
-
-    public boolean hasField (String fieldName) {
-        for (Field personalField : personalFields) {
-            if (personalField.getName().equals(fieldName)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
--- src/main/java/Model/Account/Admin.java
+++ src/main/java/Model/Account/Admin.java
@@ -8,4 +8,9 @@ public class Admin extends Account{
     private static ArrayList<Off> allOffs = Off.getAllOffs();
     private static ArrayList<Account> allAccounts = Account.getAllAccounts();
     private static ArrayList<Category> allCategories = Category.getAllCategories();
+
+    public Admin(String username, String password, String name, String lastName, String email, String phoneNumber) {
+        super(username, password, name, lastName, email, phoneNumber);
+
+    }
 }
