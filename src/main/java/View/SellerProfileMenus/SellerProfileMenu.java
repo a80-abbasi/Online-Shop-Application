858e28487ee132828cc228a2e688cba861806b92
teshnizi2 <mohammadrezaahmaditeshnizi@gamil.com>
2020-04-15 02:29:42 +0430

    adding SellerProfileMenu class methods and ManageProductsMenu class with methods and ViewOffsMenu class with methods

--- src/main/java/View/SellerProfileMenus/SellerProfileMenu.java
+++ src/main/java/View/SellerProfileMenus/SellerProfileMenu.java
@@ -2,12 +2,75 @@ package View.SellerProfileMenus;
 
 import Model.Account.Seller;
 import View.Menu;
+import View.ViewPersonalInfoMenu;
+
+import java.util.ArrayList;
 
 public class SellerProfileMenu extends Menu {
     private Seller seller;
 
-    public SellerProfileMenu(Seller seller) {
-        super("Seller Profile Menu", null);
+    public SellerProfileMenu(Seller seller, Menu parentMenu) {
+        super("Seller Profile Menu", parentMenu);
+        ArrayList<Menu> subMenus = new ArrayList<>();
+        subMenus.add(new ViewPersonalInfoMenu(seller, "seller", parentMenu));
+        subMenus.add(getViewCompanyInformationMenu());
+        subMenus.add(getViewSalesHistoryMenu());
+        subMenus.add(new ManageProductsMenu(parentMenu));
+        subMenus.add(getAddProductMenu());
+        subMenus.add(getRemoveProductMenu());
+        subMenus.add(getShowCategoriesMenu());
+        subMenus.add(new ViewOffsMenu(parentMenu));
+        subMenus.add(getViewBalanceMenu());
         this.seller = seller;
+        this.setSubmenus(subMenus);
+    }
+
+    public Menu getViewCompanyInformationMenu() {
+        return new Menu("View Company Information Menu", this) {
+            @Override
+            public void execute() {
+                parentMenu.execute();
+            }
+        };
+    }
+    public Menu getViewSalesHistoryMenu() {
+        return new Menu("View Sales History Menu", this) {
+            @Override
+            public void execute() {
+                parentMenu.execute();
+            }
+        };
+    }
+    public Menu getAddProductMenu() {
+        return new Menu("Add Product Menu", this) {
+            @Override
+            public void execute() {
+                parentMenu.execute();
+            }
+        };
+    }
+    public Menu getRemoveProductMenu() {
+        return new Menu("Remove Product Menu", this) {
+            @Override
+            public void execute() {
+                parentMenu.execute();
+            }
+        };
+    }
+    public Menu getShowCategoriesMenu() {
+        return new Menu("getShow Categories Menu", this) {
+            @Override
+            public void execute() {
+                parentMenu.execute();
+            }
+        };
+    }
+    public Menu getViewBalanceMenu() {
+        return new Menu("View Balance Menu", this) {
+            @Override
+            public void execute() {
+                parentMenu.execute();
+            }
+        };
     }
 }
