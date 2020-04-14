e2fae504a45e66aefb17a14c0ca9bd5ecb015051
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-04-15 03:02:27 +0430

    Completing inner Menus in ManageCategoriesMenu.java

--- src/main/java/View/AdminMenus/ManageCategoriesMenu.java
+++ src/main/java/View/AdminMenus/ManageCategoriesMenu.java
@@ -1,29 +1,50 @@
 package View.AdminMenus;
 
+import Controller.AdminProfileManager;
 import View.Menu;
 
 import java.util.ArrayList;
 
 public class ManageCategoriesMenu extends Menu {
+    private AdminProfileManager adminProfileManager;
 
-    public ManageCategoriesMenu(Menu parentMenu) {
+    public ManageCategoriesMenu(Menu parentMenu, AdminProfileManager adminProfileManager) {
         super("Manage Categories Menu", parentMenu);
+        this.adminProfileManager = adminProfileManager;
         ArrayList<Menu> submenus = new ArrayList<>();
         submenus.add(getEditCategoryMenu());
         submenus.add(getAddCategoryMenu());
         submenus.add(getRemoveCategoryMenu());
     }
 
+    @Override
+    public void show() {
+        String allCategoriesNames = adminProfileManager.showAllCategories();
+        System.out.println(allCategoriesNames);
+    }
+
     private Menu getEditCategoryMenu() {
         return new Menu("Edit Category", this) {
             @Override
             public void show() {
-                super.show();
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter category name to edit or (Back) to return:");
             }
 
             @Override
             public void execute() {
-                super.execute();
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    String categoryName = input;
+                    String changeField = scanner.nextLine();
+                    adminProfileManager.editCategory(categoryName, changeField);
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
@@ -32,12 +53,25 @@ public class ManageCategoriesMenu extends Menu {
         return new Menu("Add Category", this) {
             @Override
             public void show() {
-                super.show();
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter category name to add or (Back) to return:");
             }
 
             @Override
             public void execute() {
-                super.execute();
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    String categoryName = input;
+                    System.out.println("Enter special properties:");
+                    String specialProperties = scanner.nextLine();
+                    adminProfileManager.addCategory(categoryName, specialProperties);
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
@@ -46,12 +80,23 @@ public class ManageCategoriesMenu extends Menu {
         return new Menu("Remove Category", this) {
             @Override
             public void show() {
-                super.show();
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter category name to remove or (Back) to return:");
             }
 
             @Override
             public void execute() {
-                super.execute();
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    String categoryName = input;
+                    adminProfileManager.removeCategory(categoryName);
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
