e441e825db45216d80bde0d1771073506242d4ad
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-04-15 02:13:49 +0430

    Completing submenus for ManageUsersMenu.java

--- src/main/java/View/AdminMenus/ManageUsersMenu.java
+++ src/main/java/View/AdminMenus/ManageUsersMenu.java
@@ -1,5 +1,6 @@
 package View.AdminMenus;
 
+import Controller.AdminProfileManager;
 import Model.Account.Admin;
 import View.Menu;
 
@@ -7,8 +8,9 @@ import java.util.ArrayList;
 
 public class ManageUsersMenu extends Menu {
     private Admin admin;
+    private AdminProfileManager adminProfileManager;
 
-    public ManageUsersMenu(Menu parentMenu, Admin admin) {
+    public ManageUsersMenu(Menu parentMenu, Admin admin, AdminProfileManager adminProfileManager) {
         super("Manage Users Menu", parentMenu);
         ArrayList<Menu> submenus = new ArrayList<>();
         submenus.add(getViewUserMenu());
@@ -18,11 +20,32 @@ public class ManageUsersMenu extends Menu {
         this.admin = admin;
     }
 
+    @Override
+    public void show() {
+        String allUserNames = adminProfileManager.viewAllUsers();
+        System.out.println(allUserNames);
+    }
+
     public Menu getViewUserMenu() {
         return new Menu("View User", this) {
             @Override
-            public void execute() {
+            public void show() {
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter (Username) or (Back) to return:");
+            }
 
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    String userInfo = adminProfileManager.viewUser(input);
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
@@ -30,8 +53,27 @@ public class ManageUsersMenu extends Menu {
     public Menu getChangeTypeOfUserMenu() {
         return new Menu("Change Type of User", this) {
             @Override
-            public void execute() {
+            public void show() {
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter Username :");
+            }
 
+            @Override
+            public void execute() {
+                String username = scanner.nextLine();
+                System.out.println("Enter the role you want this user to have:");
+                String role = scanner.nextLine();
+                adminProfileManager.changeTypeOfAccount(username, role);
+                System.out.println("Enter (Back) to return or (Change Type) to change another user's type:");
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
@@ -39,8 +81,23 @@ public class ManageUsersMenu extends Menu {
     public Menu getDeleteUserMenu() {
         return new Menu("Delete User", this) {
             @Override
-            public void execute() {
+            public void show() {
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter Username or (Back) to return:");
+            }
 
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    adminProfileManager.deleteUser(input);
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
@@ -48,8 +105,36 @@ public class ManageUsersMenu extends Menu {
     public Menu getCreateManagerProfileMenu() {
         return new Menu("Create Manager Profile", this) {
             @Override
-            public void execute() {
+            public void show() {
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter Following data to create manager profile:");
+            }
 
+            @Override
+            public void execute() {
+                System.out.println("Enter username:");
+                String username = scanner.nextLine();
+                System.out.println("Enter password:");
+                String password = scanner.nextLine();
+                System.out.println("Enter name:");
+                String name = scanner.nextLine();
+                System.out.println("Enter lastName:");
+                String lastName = scanner.nextLine();
+                System.out.println("Enter email:");
+                String email = scanner.nextLine();
+                System.out.println("Enter phone number:");
+                String phoneNumber = scanner.nextLine();
+                adminProfileManager.createManagerProfile(username, password, name, lastName, email, phoneNumber);
+                System.out.println("Enter (Back) to return or (Create Manager Profile) to create another manager profile:");
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
