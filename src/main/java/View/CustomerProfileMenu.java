aee373c1580fc8b451595b6b9e48b7ccc02fb135
teshnizi2 <mohammadrezaahmaditeshnizi@gamil.com>
2020-04-14 20:29:00 +0430

    minor changes in CustomerProfileMenu

--- src/main/java/View/CustomerProfileMenu.java
+++ src/main/java/View/CustomerProfileMenu.java
@@ -10,14 +10,15 @@ public class CustomerProfileMenu extends Menu{
     public CustomerProfileMenu(Customer customerInCustomerProfileMenu, Menu parentMenu) {
         super("CustomerProfile Menu", parentMenu);
         ArrayList<Menu> subMenus = new ArrayList<>();
-        submenus.add(new getViewPersonalInfo(this)); //todo: w8 answer in group to change it to father function or use it in every menu;
-        submenus.add(new getViewCartMenu(this));
-        submenus.add(new getViewOrdersMenu(customerInCustomerProfileMenu,this));
+        submenus.add(new ViewPersonalInfoMenu(customer, "customer", parentMenu)); //todo: w8 answer in group to change it to father function or use it in every menu;
+        submenus.add(new ViewCartMenu(this));
+        submenus.add(new ViewOrdersMenu(customerInCustomerProfileMenu,this));
         submenus.add(getViewBalanceMenu());
         submenus.add(getViewDiscountCodesMenu());
         this.setSubmenus(subMenus);
         customer = customerInCustomerProfileMenu;
     }
+
 
     private Menu getViewBalanceMenu() {
         return new Menu("View Balance", this) {
