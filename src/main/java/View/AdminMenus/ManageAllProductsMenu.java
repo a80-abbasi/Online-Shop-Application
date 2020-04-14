19b85e65d264693b29edc1d23d4fffe200cf70bf
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-04-15 02:23:44 +0430

    Completing inner Menus in ManageAllProductsMenu.java

--- src/main/java/View/AdminMenus/ManageAllProductsMenu.java
+++ src/main/java/View/AdminMenus/ManageAllProductsMenu.java
@@ -1,27 +1,40 @@
 package View.AdminMenus;
 
+import Controller.AdminProfileManager;
 import View.Menu;
 
 import java.util.ArrayList;
 
 public class ManageAllProductsMenu extends Menu {
+    private AdminProfileManager adminProfileManager;
 
-    public ManageAllProductsMenu(Menu parentMenu) {
+    public ManageAllProductsMenu(Menu parentMenu, AdminProfileManager adminProfileManager) {
         super("Manage All Products Menu", parentMenu);
+        this.adminProfileManager = adminProfileManager;
         ArrayList<Menu> submenus = new ArrayList<>();
-
+        submenus.add(getRemoveProductMenu());
     }
 
     private Menu getRemoveProductMenu() {
         return new Menu("Remove Product", this) {
             @Override
             public void show() {
-                super.show();
+                System.out.println(this.getName() + ":");
+                System.out.println("Enter product id to remove product or (Back) to return:");
             }
 
             @Override
             public void execute() {
-                super.execute();
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                }
+                else {
+                    adminProfileManager.removeProduct(input);
+                    this.show();
+                    this.execute();
+                }
             }
         };
     }
