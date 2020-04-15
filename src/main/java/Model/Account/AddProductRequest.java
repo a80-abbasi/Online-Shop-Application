6b77865e843e2caefa243ff6539f722252bd206e
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-04-16 01:55:17 +0430

    Creating Request and AddProductRequest classes

---
+++ src/main/java/Model/Account/AddProductRequest.java
@@ -0,0 +1,31 @@
+package Model.Account;
+
+import Model.Product.Product;
+import Model.Product.ProductStatus;
+
+public class AddProductRequest extends Request {
+    private String productId;
+    private ProductStatus productStatus;
+    private String productName;
+    private String companyName;
+    private double price;
+    private int existingNumber;
+    private Seller productSeller;
+
+    public AddProductRequest(String productId, ProductStatus productStatus, String productName, String companyName,
+                             double price, int existingNumber, Seller productSeller) {
+        super("add_product_" + allRequests.size());
+        this.productId = productId;
+        this.productStatus = productStatus;
+        this.productName = productName;
+        this.companyName = companyName;
+        this.price = price;
+        this.existingNumber = existingNumber;
+        this.productSeller = productSeller;
+    }
+
+    @Override
+    public void acceptRequest() {
+        new Product(productId, productStatus, productName, companyName, price, productSeller, existingNumber);
+    }
+}
---
+++ src/main/java/Model/Account/Request.java
@@ -0,0 +1,42 @@
+package Model.Account;
+
+import java.util.ArrayList;
+
+public class Request {
+    protected static ArrayList<Request> allRequests;
+    protected String requestId;
+
+    static {
+        allRequests = new ArrayList<>();
+    }
+
+    public Request(String requestId) {
+        this.requestId = requestId;
+        allRequests.add(this);
+    }
+
+    public static ArrayList<Request> getAllRequests() {
+        return allRequests;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void acceptRequest() {
+
+    }
+
+    public static Request getRequestById(String requestId) {
+        for (Request request : allRequests) {
+            if (request.getRequestId().equals(requestId)) {
+                return request;
+            }
+        }
+        return null;
+    }
+
+    public static void removeRequest(Request request) {
+        allRequests.remove(request);
+    }
+}
