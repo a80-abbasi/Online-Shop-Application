b74c02b8fe81c09a74a266305ea5602709638134
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-04-15 15:02:46 +0430

    Completing ProfileManager.java

--- src/main/java/Controller/ProfileManager.java
+++ src/main/java/Controller/ProfileManager.java
@@ -1,12 +1,38 @@
 package Controller;
 
+import Model.Account.Account;
+
 public class ProfileManager {
+    private Account account;
 
-    public void viewPersonalInfo() {
-
+    public ProfileManager(Account account) {
+        this.account = account;
     }
 
-    public void editFieldOfProfile(String field) {
+    public String viewPersonalInfo() {
+        return account.toString();
+    }
 
+    public void editFieldOfProfile(String fieldName, String fieldChange) {
+        if (fieldName.equals("username")) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            if (fieldName.equals("password")) {
+                account.setPassword(fieldChange);
+            }
+            else if (fieldName.equals("name")) {
+                account.setName(fieldChange);
+            }
+            else if (fieldName.equals("lastName")) {
+                account.setLastName(fieldChange);
+            }
+            else if (fieldName.equals("email")) {
+                account.setEmail(fieldChange);
+            }
+            else if (fieldName.equals("phone number")) {
+                account.setPhoneNumber(fieldChange);
+            }
+        }
     }
 }
