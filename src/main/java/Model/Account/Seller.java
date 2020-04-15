1043d6bdda14d619ee5c124dcf8c4940ec7a1207
teshnizi2 <mohammadrezaahmaditeshnizi@gamil.com>
2020-04-15 15:42:27 +0430

    add balance to Seller

--- src/main/java/Model/Account/Seller.java
+++ src/main/java/Model/Account/Seller.java
@@ -6,13 +6,15 @@ import java.util.ArrayList;
 
 public class Seller extends Account {
     private String nameOfCompany;
+    private int balance;
     private ArrayList<SellLog> sellLogs;
     private ArrayList<Product> products;
     private ArrayList<Off> offs;
 
-    public Seller(String username, String password, String name, String lastName, String email, String phoneNumber, String nameOfCompany) {
+    public Seller(String username, String password, String name, String lastName, String email, String phoneNumber, String nameOfCompany, int balance) {
         super(username, password, name, lastName, email, phoneNumber);
         this.nameOfCompany = nameOfCompany;
+        this.balance = balance;
         sellLogs = new ArrayList<>();
         products = new ArrayList<>();
         offs = new ArrayList<>();
