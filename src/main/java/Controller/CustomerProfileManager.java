13fdc1d5235924bd84ea8f5d5c986321f79b140a
teshnizi2 <mohammadrezaahmaditeshnizi@gamil.com>
2020-04-15 15:39:26 +0430

    adding SellerProfileManager and CustomerProfileManager functions

---
+++ src/main/java/Controller/CustomerProfileManager.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import Model.Account.Account;
+import Model.Account.Discount;
+import Model.Product.Product;
+
+import java.util.ArrayList;
+
+public class CustomerProfileManager {
+
+    public ArrayList<Product> showProducts(Account account) {
+        return null;
+    }
+    public ArrayList<String> viewProduct(String id) {
+        return null;
+    }
+    public boolean increaseProductInCart(String id) {
+        return true;
+    }
+    public boolean decreaseProductInCart(String id) {
+        return true;
+    }
+    public int showTotalPrice(Account account)  {
+        return 0;
+    }
+    public boolean purchase(ArrayList<String> properties, String discountCode) {
+        return true;
+    }
+    public ArrayList<String> showOrders(String id) {
+        return null;
+    }
+    public boolean rateProduct(String id, int score) {
+        return true;
+    }
+    public ArrayList<Discount> viewDiscountCodes(Account account) {
+        return null;
+    }
+    public int viewBalance(Account account) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/Controller/SellerProfileManager.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import Model.Account.Account;
+import Model.Account.Off;
+import Model.Product.Category;
+import Model.Product.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SellerProfileManager {
+
+    public ArrayList<String> viewCompanyInformation(Account account) {
+        return null;
+    }
+    public ArrayList<String> viewSalesHistory(Account account) {
+        return null;
+    }
+    public ArrayList<Product> viewProductByID(String id) {
+        return null;
+    }
+    public ArrayList<Account> viewBuyersByID(String id) {
+        return null;
+    }
+    public void editByID(String id, HashMap< String , String > newFields)  {
+
+    }
+    public boolean addProduct(ArrayList<String> properties) {
+        return true;
+    }
+    public void removeProduct(String id) {
+
+    }
+    public ArrayList<Category> showAllCategories() {
+        return null;
+    }
+    public ArrayList<Off> viewOffs(Account Seller) {
+        return null;
+    }
+    public Off viewOff(String id) {
+        return null;
+    }
+    public void editOff(String id) {
+
+    }
+    public void addOff (ArrayList<String> properties) {
+
+    }
+    public int viewBalance(Account account) {
+        return 0;
+    }
+
+}
