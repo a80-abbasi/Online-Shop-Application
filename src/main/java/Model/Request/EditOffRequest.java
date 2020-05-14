65e359688601a7bf1dca01b3c861b3ba49d35f5c
Ali <a80.abbasi@yahoo.com>
2020-05-14 21:49:12 +0430

    serializing EditOffRequest with XML

--- src/main/java/Model/Request/EditOffRequest.java
+++ src/main/java/Model/Request/EditOffRequest.java
@@ -2,16 +2,27 @@ package Model.Request;
 
 import Model.Account.Off;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class EditOffRequest extends Request {
+    private static ArrayList<EditOffRequest> allEditOffRequests = new ArrayList<>();
     private Off off;
     private HashMap<String, String> fieldChanges;
 
     public EditOffRequest(Off off, HashMap<String, String> fieldChanges) {
         super("edit_off_" + allRequests.size(), RequestType.Editing_Off_Request);
+        allEditOffRequests.add(this);
         this.off = off;
         this.fieldChanges = fieldChanges;
+    }
+
+    public static ArrayList<EditOffRequest> getAllEditOffRequests() {
+        return allEditOffRequests;
+    }
+
+    public static void setAllEditOffRequests(ArrayList<EditOffRequest> allEditOffRequests) {
+        EditOffRequest.allEditOffRequests = allEditOffRequests;
     }
 
     @Override
