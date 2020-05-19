12dd6fd89625acb2169fa0c2424ee05d87c8a0eb
teshnizi2 <mohammadrezaahmaditeshnizi@gamil.com>
2020-05-20 00:28:42 +0430

    change ViewOrdersMenu

--- src/main/java/View/CustomerProfileMenus/ViewOrdersMenu.java
+++ src/main/java/View/CustomerProfileMenus/ViewOrdersMenu.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 public class ViewOrdersMenu extends Menu {
     private CustomerProfileManager customerProfileManager;
+    private CustomerProfileMenu customerProfileMenu;
     public ViewOrdersMenu(Customer customer, Menu parentMenu) {
         super("View Orders Menu", parentMenu);
         ArrayList<Menu> subMenus = new ArrayList<>();
@@ -19,7 +20,10 @@ public class ViewOrdersMenu extends Menu {
 
     @Override
     public void show() {
-        System.out.println(customerProfileManager.showOrdersSellerNameAndDate()); // todo: wrong input if (null)
+        try {
+            System.out.println(customerProfileManager.showOrdersSellerNameAndDate()); // todo: wrong input if (null)
+        } catch (Exception e) {
+        }
         super.show();
     }
 
@@ -30,27 +34,12 @@ public class ViewOrdersMenu extends Menu {
                 System.out.println(this.getName() + ":");
                 System.out.println("Enter orderID to view order:");
             }
-
             @Override
             public void execute() {
                 show();
-                while (true) {
-                    String input = scanner.nextLine();
-                    if (input.equalsIgnoreCase("Back")) {
-                        this.parentMenu.execute();
-                    } else if (input.equalsIgnoreCase("Logout")) {
-                        loginAndRegisterManager.logoutUser();
-                    } else if (input.matches("\\A\\d+\\z")) {
-                        if (customerProfileManager.isInputValidForBuyLogID(input)) { //todo: wrong input if (null)
-                            System.out.println(customerProfileManager.showOrder(input));
-                            parentMenu.execute();
-                        } else {
-                            System.out.println("There is no order with this ID");
-                        }
-                    } else {
-                        System.out.println("Please enter valid number:");
-                    }
-                }
+                String input = customerProfileMenu.getBuyLogInView(parentMenu);
+                System.out.println(customerProfileManager.showOrder(input));
+                parentMenu.execute();
             }
         };
     }
@@ -62,46 +51,13 @@ public class ViewOrdersMenu extends Menu {
                 System.out.println(this.getName() + ":");
                 System.out.println("Enter productID:");
             }
-
             @Override
             public void execute() {
                 show();
-                while (true) {
-                    String input = scanner.nextLine();
-                    if (input.equalsIgnoreCase("Back")) {
-                        this.parentMenu.execute();
-                    } else if (input.equals("Logout")) {
-                        loginAndRegisterManager.logoutUser();
-                    } else if (input.matches("\\A\\d+\\z")) {
-                        if (ProductsManager.isValidInputForProductID(input)) {
-                            System.out.println("Enter rate in range (1-5):");
-                            String productID = input;
-                            while(true) {
-                                input = scanner.nextLine();
-                                if (input.equalsIgnoreCase("Back")) {
-                                    this.show();
-                                    break;
-                                } else if (input.equalsIgnoreCase("Logout")) {
-                                    loginAndRegisterManager.logoutUser();
-                                } else if (input.matches("\\A\\d+\\z")) {
-                                    if (Integer.parseInt(input) <= 5 && Integer.parseInt(input) >= 1) {
-                                        customerProfileManager.rateProduct(productID, Integer.parseInt(input));
-                                        System.out.println("Your rate submitted");
-                                        parentMenu.execute();
-                                    } else {
-                                        System.out.println("please enter a number in range(1-5):");
-                                    }
-                                } else {
-                                    System.out.println("please enter valid number:");
-                                }
-                            }
-                        } else {
-                            System.out.println("There is no product with this ID");
-                        }
-                    } else {
-                        System.out.println("Please enter valid number:");
-                    }
-                }
+                String productID = customerProfileMenu.getProductIdInView(parentMenu);
+                String score = customerProfileMenu.getProductRateInView(this);
+                customerProfileManager.rateProduct(productID, score);
+                System.out.println("Your rate submitted");
             }
         };
     }
