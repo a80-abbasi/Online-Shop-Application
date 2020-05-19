c27bd2e1de2fcbebf6a4fdc533d7da0ce2828c39
teshnizi2 <mohammadrezaahmaditeshnizi@gamil.com>
2020-05-20 00:30:41 +0430

    add and change functions in CustomerProfileManager

--- src/main/java/Controller/CustomerProfileManager.java
+++ src/main/java/Controller/CustomerProfileManager.java
@@ -3,10 +3,13 @@ package Controller;
 import Model.Account.*;
 import Model.Product.Product;
 import Model.Product.Score;
+import View.Menu;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+
+import static Controller.ProductsManager.getTotalPrice;
 
 public class CustomerProfileManager extends ProfileManager{
     private Customer customer;
@@ -71,7 +74,8 @@ public class CustomerProfileManager extends ProfileManager{
         return customer.getBuyLogByID(id);
     }
 
-    public void rateProduct(String id, int intScore) {
+    public void rateProduct(String id, String stringScore) {
+        int intScore = Integer.parseInt(stringScore);
         Score score = new Score(customer, Product.getProductByID(id), intScore);
         Product.getProductByID(id).getAllScores().add(score);
     }
@@ -80,14 +84,9 @@ public class CustomerProfileManager extends ProfileManager{
         return null;
     }
 
-    public int viewBalance() {
+    public double viewBalance() {
         return customer.getBalance();
     }
-
-    /*public boolean isDiscountCodeAvailableForCustomer(Discount discount) {
-        //todo: ali! complete this please!
-        return true;
-    }*/
 
     public static int getExistingNumberOfProductInStore(Product product, int number) {
         return product.getExistingNumber();
@@ -97,68 +96,96 @@ public class CustomerProfileManager extends ProfileManager{
         return Customer.getCustomerFieldsForPurchase();
     }
 
-    public MyResult areNewReceivedFieldsValueValid(HashMap<String, String> receivedFields) {
+    public Menu.MyResult areNewReceivedFieldsValueValid(HashMap<String, String> receivedFields) {
         boolean valid = true;
         String message = "Your information submitted";
+        String secondMessage = "";
         if (!receivedFields.get("name").matches("\\A\\w+\\z")) {
             valid = false;
             message = "please enter a valid name";
+            secondMessage = "1";
         } else if (!receivedFields.get("name").matches("\\A\\w+\\z")) {
             valid = false;
             message = "please enter a valid lastName";
+            secondMessage = "2";
         } else if (!receivedFields.get("lastName").matches("\\A\\w+\\z")) {
             valid = false;
             message = "please enter a valid name";
+            secondMessage = "3";
         } else if (!receivedFields.get("phoneNumber").matches("\\A\\d+\\z")) {
             valid = false;
             message = "please enter a valid phoneNumber";
+            secondMessage = "4";
         } else if (!receivedFields.get("email").matches("@gmail.com\\z")) {
             valid = false;
             message = "please enter a valid email";
+            secondMessage = "5";
         } else if (!receivedFields.get("PostCode").matches("\\A\\d+\\z")) {
             valid = false;
             message = "please enter a valid PostCode";
+            secondMessage = "6";
         }
-        return new MyResult(valid,message);
+        return new Menu.MyResult(valid, message, "");
     }
 
     public ArrayList<Discount> getDiscountCodes() {
         return customer.getAllDiscountCodesForCustomer();
     }
 
-    public static boolean isDiscountCodeValid(String discountCode) {
+    public static Menu.MyResult isDiscountCodeValid(String discountCode) {
         Discount discount = Discount.getDiscountByDiscountCode(discountCode);
         if (discount == null){
-            return false;
+            return new Menu.MyResult(false, "There is no discount with this ID", "") ;
         }
-        if (discount.getIncludingCustomers().contains((Customer) Account.getLoggedInAccount())){
-            Date date = new Date();
-            return date.compareTo(discount.getStartTime()) >= 0 && date.compareTo(discount.getEndTime()) <= 0;
+        if (!discount.getIncludingCustomers().contains((Customer) Account.getLoggedInAccount())) {
+            return new Menu.MyResult(false, "You cant use this discount!", "");
         }
-        return false;
+        Date date = new Date();
+        if(!(date.compareTo(discount.getStartTime()) >= 0 && date.compareTo(discount.getEndTime()) <= 0)) {
+            return new Menu.MyResult(false, "This discountID is not up now!", "");
+        }
+        return new Menu.MyResult(true, "Your discount code submitted", discountCode);
     }
 
-    public double costCalculator(String discountCode) {
-        double lastPrice = 0;
-        lastPrice += customer.getTotalPrice();
-        //todo: ali! complete this!
-        //lastPrice -= lastPrice * off;
-        lastPrice -= lastPrice * (Discount.getDiscountByDiscountCode(discountCode).getDiscountPercent());
-        return lastPrice;
+    public double costCalculatorWithOff() {
+        double priceWithOff = 0.0;
+        for (Product product: customer.getCart().keySet()) {
+            priceWithOff += product.getPriceWithOff();
+        }
+        return priceWithOff;
+    }
+    public double costCalculatorWithOffAndDiscount(String discountCode) {
+        double priceWithOffAndDiscount = 0.0;
+        Discount discount;
+        for (Product product: customer.getCart().keySet()) {
+            priceWithOffAndDiscount += product.getPriceWithOff();
+        }
+        if(!discountCode.equals("")) {
+            discount = Discount.getDiscountByDiscountCode(discountCode);
+            priceWithOffAndDiscount -= priceWithOffAndDiscount * discount.getDiscountPercent();
+        }
+        return priceWithOffAndDiscount;
     }
 
-//    public void doingsAfterBuyProducts(double cost, String UsedDiscountCode) {
-//        customer.getCart();
-//        for (Product product : customer.getCart().keySet()) {
-//            product.setExistingNumber(product.getExistingNumber());
-//            product.setVisitNumber();
-//            product.setProductStatus();
-//        }
-//        customer.setCart();
-//        customer.setBalance();
-//        customer.setUsedDiscounts();
-        //todo: complete this please
-//    }
+    public void doingsAfterBuyProducts(double price, String usedDiscountCode) {
+        customer.setBalance(customer.getBalance() - price);
+        setUsedDiscountCodes(usedDiscountCode);
+        addBuyLog(price);
+        //System.out.println(checkForDiscountGift());
+        for (Product product : customer.getCart().keySet()) {
+            Seller seller = product.getProductSeller();
+            int productNewExistingNumber = product.getExistingNumber() - customer.getCart().get(product);
+            seller.setBalance(seller.getBalance() + product.getPriceWithOff()); //seller setBalance
+            product.setExistingNumber(productNewExistingNumber); //Product setExistingNumber
+            if (!product.getProductBuyers().contains(customer))
+                product.getProductBuyers().add(customer); //Product setProductBuyers
+        }
+        //discount.getDiscountPerCustomer();
+        //todo: check discount code use less than
+
+        //Sell log
+
+    }
 
     public boolean canCustomerPay(double cost) {
         if (customer.getBalance() >= cost) {
@@ -175,22 +202,39 @@ public class CustomerProfileManager extends ProfileManager{
         return false;
     }
 
-    public final class MyResult {
-        private final boolean valid;
-        private final String  message;
+    public boolean isInputInteger(String input) {
+        return input.matches("\\A\\d+\\z");
+    }
 
-        public MyResult(boolean valid, String message) {
-            this.valid = valid;
-            this.message = message;
-        }
 
-        public boolean getValid() {
-            return valid;
+    public void setProductExistingNumber() {
+        for (Product product : customer.getCart().keySet()) {
+            Seller seller = product.getProductSeller();
+            seller.setBalance(seller.getBalance() + product.getPriceWithOff());
         }
+    }
 
-        public String getMessage() {
-            return message;
+    public void setUsedDiscountCodes(String usedDiscountCode) {
+        if (!usedDiscountCode.equals("")) {
+            Discount discount =  Discount.getDiscountByDiscountCode(usedDiscountCode);
+            if (customer.getUsedDiscounts().containsKey(discount)) {
+                customer.getUsedDiscounts().put(discount, customer.getUsedDiscounts().get(discount) + 1);
+            } else {
+                customer.getUsedDiscounts().put(discount, 1);
+            }
         }
+    }
+
+    public void addBuyLog(double price) { //todo
+//        String buyLogID = customer.getUsername() + customer.getBuyLogs().size();
+//        BuyLog buyLog = new BuyLog(buyLogID, new Date(), price, getTotalPrice() - price, /*todo*/);
+//        customer.getBuyLogs().add(buyLog);
+    }
+
+    public void addSellLog() { //todo
+//        String buyLogID = customer.getUsername() + customer.getBuyLogs().size();
+//        BuyLog buyLog = new BuyLog(buyLogID, new Date(), price, getTotalPrice() - price, /*todo*/);
+//        customer.getBuyLogs().add(buyLog);
     }
 
 }
