c160b55e506a35d6903226332cb2785e95aace46
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-06-24 16:58:45 +0430

    Adding getRequestDetails() to all kinds of Request (Incomplete)

--- src/main/java/Model/Request/RemoveProductRequest.java
+++ src/main/java/Model/Request/RemoveProductRequest.java
@@ -1,6 +1,9 @@
 package Model.Request;
 
 import Model.Product.Product;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.ArrayList;
 
@@ -38,5 +41,22 @@ public class RemoveProductRequest extends Request {
                 ", requestId='" + requestId + '\'' +
                 ", requestType=" + requestType +
                 '}';
+    }
+
+    @Override
+    public TableView getRequestDetails() {
+        TableView requestDetails = new TableView();
+
+        TableColumn<String, Request> column1 = new TableColumn<>("Request ID");
+        column1.setCellValueFactory(new PropertyValueFactory<>("requestId"));
+
+        TableColumn<RequestType, Request> column2 = new TableColumn<>("Request Type");
+        column2.setCellValueFactory(new PropertyValueFactory<>("requestType"));
+
+        TableColumn<Product, RemoveProductRequest> column3 = new TableColumn<>("Product");
+        column3.setCellValueFactory(new PropertyValueFactory<>("product"));
+
+        requestDetails.getItems().add(this);
+        return requestDetails;
     }
 }
