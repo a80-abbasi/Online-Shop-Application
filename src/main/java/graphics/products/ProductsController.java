dd38a9317bd7b07e66ea788b8aee464e3a361af7
Ali <a80.abbasi@yahoo.com>
2020-06-26 13:26:40 +0430

    ProductsMenu opens product Comparison page

--- src/main/java/graphics/products/ProductsController.java
+++ src/main/java/graphics/products/ProductsController.java
@@ -6,6 +6,8 @@ import Model.Product.Category;
 import Model.Product.Product;
 import graphics.App;
 import graphics.ToggleSwitch;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -21,6 +23,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import org.controlsfx.control.Rating;
 
 import java.io.IOException;
@@ -56,6 +59,7 @@ public class ProductsController {
     public ImageView magnifier3;
     public TextField filterBySellerTextField;
     public TextField filterByCompanyTextField;
+    public Label messageLabel;
 
     private ArrayList<Product> showingProducts;
     private ProductsManager productsManager;
@@ -67,6 +71,9 @@ public class ProductsController {
     private static final int numberOfProductsPerPage = 10;
     private boolean disableSearchIsHidden = true;
     private static String parentAddress = "mainMenu";
+    private boolean isComparisonPage;
+    private Product firstProductForComparison;
+    private StackPane firstProductStackPane;
 
     public void initialize() {
         productsManager = new ProductsManager();
@@ -94,6 +101,18 @@ public class ProductsController {
         ProductPageController.setCartButton(cartImage);
         App.setBackButton(backImage, parentAddress);
         ProductPageController.setProfileButton(profileImage, "productsMenu");
+    }
+
+    public void prepareForComparison(Product firstProductForComparison, StackPane firstProductStackPane){
+        this.firstProductForComparison = firstProductForComparison;
+        this.firstProductStackPane = firstProductStackPane;
+        isComparisonPage = true;
+        messageLabel.setText("Select Second Product for Comparison");
+        messageLabel.setOpacity(1);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3), event -> messageLabel.setOpacity(0)));
+        timeline.setCycleCount(1);
+        timeline.play();
+        showProducts();
     }
 
     private void setFilterBySellerAndCompany() {
@@ -295,8 +314,29 @@ public class ProductsController {
         vBox.setPrefHeight(280);
         vBox.setSpacing(8);
         shadowOnMouseHover(vBox);
-        vBox.setOnMouseClicked(event -> openProduct(product, stackPane));
+        vBox.setOnMouseClicked(event -> {
+            if (isComparisonPage){
+                goToComparisonPage(product, stackPane);
+            }
+            else {
+                openProduct(product, stackPane);
+            }
+        });
         return vBox;
+    }
+
+    private void goToComparisonPage(Product secondProduct, StackPane secondProductStackPane){
+        try {
+            ComparisonPageController comparisonPageController = ((ComparisonPageController)App.setRoot("comparisonPage"));
+            comparisonPageController.setProduct1(firstProductForComparison);
+            comparisonPageController.setProduct2(secondProduct);
+            comparisonPageController.setFirstProductImageStackPane(firstProductStackPane);
+            comparisonPageController.setSecondProductImageStackPane(secondProductStackPane);
+
+            comparisonPageController.setEveryThing();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void openProduct(Product product, StackPane imageStackPane){
