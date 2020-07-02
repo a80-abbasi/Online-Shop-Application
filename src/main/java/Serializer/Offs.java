4503b675b538c866bf1714783e18dbe031811153
Ali <a80.abbasi@yahoo.com>
2020-07-02 18:58:27 +0430

    A little change in  off

--- src/main/java/Serializer/Offs.java
+++ src/main/java/Serializer/Offs.java
@@ -1,5 +1,6 @@
 package Serializer;
 
+import Model.Account.Admin;
 import Model.Account.Off;
 
 import javax.xml.bind.JAXB;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Offs {
     private final static String pathName = "src\\main\\resources\\offs.xml";
@@ -25,6 +27,14 @@ public class Offs {
         if(file.exists() && !file.isDirectory() && file.length() > 0) {
             try(BufferedReader input = Files.newBufferedReader(Paths.get(pathName))) {
                 Offs offs = JAXB.unmarshal(input, Offs.class);
+                HashMap<String, Integer> duplicateOffs = new HashMap<>();
+                offs.allOffs.forEach(off -> duplicateOffs.put(off.getOffID(),
+                        duplicateOffs.getOrDefault(off.getOffID(), 0) + 1));
+                duplicateOffs.forEach((off, number) -> {
+                    for (int i = 0; i < number - 1; i++) {
+                        offs.allOffs.remove(Off.getOffById(off));
+                    }
+                });
                 Off.setAllOffs(offs.allOffs);
             } catch (IOException e) {
                 System.out.println("Error opening file");
