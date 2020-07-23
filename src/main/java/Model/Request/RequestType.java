d9608a60851301d35be67dd273825cf6174a6803
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-07-23 21:35:22 +0430

    Adding RegisterSupporterRequest.java And Completing It

--- src/main/java/Model/Request/RequestType.java
+++ src/main/java/Model/Request/RequestType.java
@@ -1,7 +1,10 @@
 package Model.Request;
 
 public enum RequestType {
-    Adding_Off_Request("Add Off Request"), Adding_Product_Request("Add Product Request"), Editing_Off_Request("Edit Off Request"), Editing_Product_Request("Edit Product Request"), Register_Seller_Request("Register Seller Request"), Remove_Product_Request("Remove Product Request");
+    Adding_Off_Request("Add Off Request"), Adding_Product_Request("Add Product Request"),
+    Editing_Off_Request("Edit Off Request"), Editing_Product_Request("Edit Product Request"),
+    Register_Seller_Request("Register Seller Request"), Remove_Product_Request("Remove Product Request"),
+    Register_Supporter_Request("Register Supporter Request");
 
     private String requestType;
 
