366bf4ee1cb97e755845475f17e4bb1d029a259a
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-07-24 17:14:12 +0430

    Adding SellerWallet and its Method Signatures

---
+++ src/main/java/graphics/SellerProfile/SellerWallet.java
@@ -0,0 +1,18 @@
+package graphics.SellerProfile;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class SellerWallet {
+    public TextField balanceField;
+    public TextField withdrawAmountField;
+    public TextField chargeAmountField;
+
+    public void withdrawFromWallet(MouseEvent mouseEvent) {
+
+    }
+
+    public void chargeWallet(MouseEvent mouseEvent) {
+
+    }
+}
