301732f10bdfc7db5f513fc84bea77ca9b10b126
Teshnizi <mohammadrezaahmaditeshnizi@gmail.com>
2020-07-24 23:11:09 +0430

    update send table

--- src/main/java/graphics/AdminProfile/ManageSaleHistory.java
+++ src/main/java/graphics/AdminProfile/ManageSaleHistory.java
@@ -60,6 +60,7 @@ public class ManageSaleHistory {
             return;
         }
         ((BuyLog) selectedBuyLog).setSendingCondition("Send");
+        allBuyLogsTable = adminProfileManager.getSaleHistoryTable(allBuyLogsTable);
     }
 
     public void ShowProducts(MouseEvent mouseEvent) {
