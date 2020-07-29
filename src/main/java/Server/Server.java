27446eef4f9c93f077cdc847f67535e834cfbe10
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-07-29 14:01:34 +0430

    Fixing server problems

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -33,6 +33,7 @@ public class Server extends Application {
     private static final Gson gson = new Gson();
     private static DataOutputStream dataOutputStream;
     private static DataInputStream dataInputStream;
+    private static HashMap<Supporter, String> CustomersInQueue;
 
     @Override
     public void start(Stage stage) {
